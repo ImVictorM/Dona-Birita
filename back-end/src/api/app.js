c0fceac4acdb224c54b12585b44d7a0cda6181d8
cybersekh <carolina.jpeg@gmail.com>
2023-03-28 19:07:15 -0300

    requisito 2

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const app = express();
+const { userRouter } = require('./routes');
 
+app.use(express.json());
+app.use('/login', userRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
