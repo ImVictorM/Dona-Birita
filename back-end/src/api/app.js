985948d64d2e88d73b1f21753ef03f87438cebda
Victor Mendes <victorutdsim7@gmail.com>
2023-03-29 12:51:04 -0300

    feat: permite acesso do front com o cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const handleError = require('./middlewares/errorHandler');
+require('express-async-errors');
 
 const app = express();
 const { userRouter } = require('./routes');
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
 app.use('/login', userRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
