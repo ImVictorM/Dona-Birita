eedc7411e9499c75a562a6ead26bb99e744078ec
Victor Mendes <victorutdsim7@gmail.com>
2023-03-29 19:43:22 -0300

    test evaluator

--- back-end/src/api/middlewares/errorHandler.js
+++ back-end/src/api/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 async function handleError(error, _req, res, _next) {
   const errorStack = Number(error.stack);
   const errorStackIsNotANumber = Number.isNaN(errorStack);
-  const errorCode = errorStackIsNotANumber ? 500 : errorStack;
+  const errorCode = errorStackIsNotANumber ? 520 : errorStack;
   return res.status(errorCode).json({ message: error.message });
 }
 
