ee394a7db9c39d9890326dcf36ef4736c284ffb9
Victor Mendes <victorutdsim7@gmail.com>
2023-03-29 12:19:24 -0300

    feat: cria middleware para lidar com erros

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handleError = require('./middlewares/errorHandler');
 
 const app = express();
 const { userRouter } = require('./routes');
@@ -6,5 +7,7 @@ const { userRouter } = require('./routes');
 app.use(express.json());
 app.use('/login', userRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(handleError);
 
 module.exports = app;
