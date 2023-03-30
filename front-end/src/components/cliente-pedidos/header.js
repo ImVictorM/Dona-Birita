2be8fb59f9bf4db2ad19052dc6fe6400751cb9ce
Rafael Rocha <rs4003193@gmail.com>
2023-03-30 16:52:24 -0300

    requisito 11 test

---
+++ front-end/src/components/cliente-pedidos/header.js
@@ -0,0 +1,12 @@
+function HeaderPedidos () {
+  return(
+    <header>
+      <nav>
+        <Link to="" data-testid="customer_products__element-navbar-link-products">Produtos</Link>
+        <Link to="" data-testid="customer_products__element-navbar-link-orders">Meus Pedidos</Link>
+        <Link to="" data-testid="customer_products__element-navbar-user-full-name"></Link>
+        <Link to="" data-testid="customer_products__element-navbar-link-logout">Sair</Link>
+      </nav>
+    </header>
+  )
+}
