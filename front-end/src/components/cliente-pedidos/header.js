0cf2fbe94cca6fcb7c312c9e10fe4ea068d40f91
Elivelton <eliveltonmn@outlook.com>
2023-03-30 17:02:04 -0300

    fix: arruma lint

--- front-end/src/components/cliente-pedidos/header.js
+++ front-end/src/components/cliente-pedidos/header.js
@@ -1,12 +1,34 @@
-function HeaderPedidos () {
-  return(
+import { Link } from 'react-router-dom';
+
+function HeaderPedidos() {
+  return (
     <header>
       <nav>
-        <Link to="" data-testid="customer_products__element-navbar-link-products">Produtos</Link>
-        <Link to="" data-testid="customer_products__element-navbar-link-orders">Meus Pedidos</Link>
-        <Link to="" data-testid="customer_products__element-navbar-user-full-name"></Link>
-        <Link to="" data-testid="customer_products__element-navbar-link-logout">Sair</Link>
+        <Link
+          to="/"
+          data-testid="customer_products__element-navbar-link-products"
+        >
+          Produtos
+        </Link>
+        <Link
+          to="/"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Meus Pedidos
+        </Link>
+        <Link
+          to="/"
+          data-testid="customer_products__element-navbar-user-full-name"
+        />
+        <Link
+          to="/"
+          data-testid="customer_products__element-navbar-link-logout"
+        >
+          Sair
+        </Link>
       </nav>
     </header>
-  )
+  );
 }
+
+export default HeaderPedidos;
