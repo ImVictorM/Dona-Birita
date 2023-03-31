c8d33dbf160fb29d2106f953d121e9ee49bd241c
Rafael Rocha <rs4003193@gmail.com>
2023-03-30 21:02:52 -0300

    requisito 12 concluido

--- front-end/src/components/cliente-pedidos/card.js
+++ front-end/src/components/cliente-pedidos/card.js
@@ -1,6 +1,17 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Card({ id, name, imag, price }) {
+  const [quantity, setQuantity] = useState(0);
+  function handleClickAdd() {
+    const result = quantity + 1;
+    setQuantity(result);
+  }
+
+  function handleClickMinus() {
+    const result = quantity - 1;
+    setQuantity(result);
+  }
   return (
     <li>
       <p data-testid={ `customer_products__element-card-title-${id}` }>{ name }</p>
@@ -10,6 +21,28 @@ function Card({ id, name, imag, price }) {
         alt={ name }
       />
       <p data-testid={ `customer_products__element-card-price-${id}` }>{ price }</p>
+      <button
+        type="button"
+        onClick={ handleClickMinus }
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+      >
+        -
+
+      </button>
+      <input
+        type="number"
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+        name="quantidade"
+        value={ quantity }
+      />
+      <button
+        type="button"
+        onClick={ handleClickAdd }
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+      >
+        +
+
+      </button>
     </li>
   );
 }
