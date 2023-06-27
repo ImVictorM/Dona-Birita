d77caed9cf1c9ea6930f7c3a8853b7a29e1ea6ca
Victor Mendes <victorutdsim7@gmail.com>
2023-06-27 17:44:05 -0300

    test: cria mock do localstorage a atualiza setup de testes

--- front-end/src/setupTests.js
+++ front-end/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import LocalStorageMock from './tests/utils/LocalStorageMock';
+
+jest.mock('./utils/requestWithCORS', () => jest.fn());
+
+global.localStorage = new LocalStorageMock;
