cb4822438a1fa29f991dd804c7d8a0a70192b313
Victor Mendes <victorutdsim7@gmail.com>
2023-08-15 18:42:39 -0300

    style: refatora css header

--- front-end/src/components/header/header.js
+++ front-end/src/components/header/header.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import './header.css';
+import styles from './header.module.scss';
 
 function Header() {
   const history = useHistory();
   const user = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <header className="main-header">
-      <nav>
+    <header className={ styles.header }>
+      <nav className={ styles.header__nav }>
         <div>
           {
             user.role === 'customer' && (
               <Link
                 to="/customer/products"
                 data-testid="customer_products__element-navbar-link-products"
+                className={ styles.header__nav__link }
               >
                 Produtos
               </Link>
@@ -24,6 +26,7 @@ function Header() {
               <Link
                 to={ `/${user.role}/orders` }
                 data-testid="customer_products__element-navbar-link-orders"
+                className={ styles.header__nav__link }
               >
                 { user.role === 'customer' ? 'Meus Pedidos' : 'Pedidos' }
               </Link>
@@ -37,12 +40,14 @@ function Header() {
         <div>
           <span
             data-testid="customer_products__element-navbar-user-full-name"
+            className={ styles.header__nav__name }
           >
             { user.name }
           </span>
           <button
             type="button"
             data-testid="customer_products__element-navbar-link-logout"
+            className={ styles.header__nav__button }
             onClick={ () => {
               localStorage.clear();
               history.push('/login');
