e0692cfd92fe9f115508d30b61388c6642f9d711
Victor Mendes <victorutdsim7@gmail.com>
2023-08-20 17:05:03 -0300

    style: refatora tela de pedidos

--- front-end/src/components/orders/orderList.js
+++ front-end/src/components/orders/orderList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { OrderContext } from '../../context/Context';
+import styles from './orderList.module.scss';
 
 function OrderList() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -13,17 +14,17 @@ function OrderList() {
   const SLICE_DATE_AT_INDEX = 10;
 
   return (
-    <section className="order-container">
+    <section className={ styles.orders }>
       {
         orders.map((order) => (
           <Link
             to={ `/${user.role}/orders/${order.id}` }
             key={ order.id }
-            className="order-card"
+            className={ styles.orders__card }
             data-testid={ `${user.role}_orders__element-order-link-${order.id}` }
           >
-            <div>
-              <span>Data</span>
+            <div className={ styles.orders__card__cell }>
+              <span className={ styles.orders__card__cell__title }>Data</span>
               <span
                 data-testid={ `${user.role}_orders__element-order-date-${order.id}` }
               >
@@ -36,35 +37,37 @@ function OrderList() {
                 }
               </span>
             </div>
-            <div>
-              <span>Pedido</span>
+            <div className={ styles.orders__card__cell }>
+              <span className={ styles.orders__card__cell__title }>Pedido</span>
               <span
                 data-testid={ `${user.role}_orders__element-order-id-${order.id}` }
               >
                 {order.id}
               </span>
             </div>
-            <div>
-              <span>Status</span>
+            <div className={ styles.orders__card__cell }>
+              <span className={ styles.orders__card__cell__title }>Status</span>
               <span
                 data-testid={ `${user.role}_orders__element-delivery-status-${order.id}` }
               >
                 {order.status}
               </span>
             </div>
-            <div>
-              <span>Total</span>
-              <span>R$</span>
-              <span
-                data-testid={ `${user.role}_orders__element-card-price-${order.id}` }
-              >
-                {order.totalPrice.replace('.', ',')}
-              </span>
+            <div className={ styles.orders__card__cell }>
+              <span className={ styles.orders__card__cell__title }>Total</span>
+              <div>
+                <span>R$: </span>
+                <span
+                  data-testid={ `${user.role}_orders__element-card-price-${order.id}` }
+                >
+                  {order.totalPrice.replace('.', ',')}
+                </span>
+              </div>
             </div>
             {
               user.role === 'seller' && (
-                <div>
-                  <span>Endereço</span>
+                <div className={ styles.orders__card__cell }>
+                  <span className={ styles.orders__card__cell__title }>Endereço</span>
                   <span
                     data-testid={ `seller_orders__element-card-address${order.id}` }
                   >
