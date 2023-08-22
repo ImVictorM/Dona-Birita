39ed436644c754d16cde20772eda35e2aea79602
Victor Mendes <victorutdsim7@gmail.com>
2023-08-21 21:52:07 -0300

    style: controladores tela de pedido

--- front-end/src/components/orderDetails/sellerControllers.js
+++ front-end/src/components/orderDetails/sellerControllers.js
@@ -1,18 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { OrderContext } from '../../context/Context';
+import styles from './sellerControllers.module.scss';
 
 function SellerControllers() {
   const { id: ID_FROM_URL } = useParams();
   const { updateOrderStatus, selectedOrder } = useContext(OrderContext);
 
   return (
-    <div>
+    <div className={ styles.order__details__controllers }>
       <button
         type="button"
         data-testid="seller_order_details__button-preparing-check"
         disabled={ selectedOrder.status !== 'Pendente' }
         onClick={ () => updateOrderStatus('Preparando', ID_FROM_URL) }
+        className={ styles.order__details__controllers__controller }
       >
         PREPARAR PEDIDOS
 
@@ -22,6 +24,7 @@ function SellerControllers() {
         data-testid="seller_order_details__button-dispatch-check"
         disabled={ selectedOrder.status !== 'Preparando' }
         onClick={ () => updateOrderStatus('Em Trânsito', ID_FROM_URL) }
+        className={ styles.order__details__controllers__controller }
       >
         SAIU PARA ENTREGA
 
